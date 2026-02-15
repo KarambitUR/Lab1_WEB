73d5cbd6652a91f0492b174553ac6d42e7b49e61
KarambitUR <misha7.9kvtecko@gmial.com>
2026-02-15 20:30:16 +0200

    лаб1

---
+++ script.js
@@ -0,0 +1,15 @@
+< !DOCTYPE html >
+    <html>
+
+        <head>
+            <meta charset="UTF-8">
+                <title>Lab 1</title>
+        </head>
+
+        <body>
+            <h1>Лабораторна робота №1</h1>
+            <p>Відкрийте консоль (F12) та використовуйте функцію triangle()</p>
+            <script src="script.js"></script>
+        </body>
+
+    </html>
